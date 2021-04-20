1bc4f2569039ebed7f7f746df30b16189c5567eb
MountVase <markus.vasemagi@gmail.com>
2021-04-20 14:40:53 +0300

    web3provider rendering logic

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+
+import { Web3ReactProvider } from '@web3-react/core'
+import { Web3Provider } from '@ethersproject/providers'
+
 import { Main } from '@aragon/ui'
+
+function getLibrary(provider) {
+  const library = new Web3Provider(provider);
+  library.pollingInterval = 8000;
+  return library;
+}
+
+
 
 ReactDOM.render(
   <React.StrictMode>
+   <Web3ReactProvider getLibrary={getLibrary} >
     <Main>
       <App />
     </Main>
+  </Web3ReactProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
