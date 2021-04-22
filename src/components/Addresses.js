2563eeaeea9416b132d872ad1924eadffacb6e9a
MountVase <markus.vasemagi@gmail.com>
2021-04-22 21:39:26 +0300

    trapping temporal whales feature

--- src/components/Addresses.js
+++ src/components/Addresses.js
@@ -1,18 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { Table, TableHeader, TableRow, TableCell, Link, TextInput, Button, Text, CircleGraph } from '@aragon/ui'
 
-const list = ["0x", "0", "ax0", "zeroXzeroBitch"]
+// this should be fetched for specific addresses
+const initialList = ["0x","0xA14964479Ebf9cD336011ad80652b08CD83dFE3A", "0xD90626F63Ddb82f39634DA594D65826120DaC01e", "0x0C18Af6D73553C481993005E78b0651dCf9C7bA3"]
 
 const Addresses = () => {
   const [text, setText] = useState('')
-  const [whales, setWhales] = useState()
-
+  const [whales, setWhales] = useState(initialList)
+ 
+  const handleClick = () => {
+    setWhales(whales => [...whales, text])
+  }
 
   return (
      <>
      <Text>Enter a new whale address that you want to trap: </Text>
       <TextInput value={text} onChange={e => setText(e.target.value)} placeholder="0x000"/>
-      <Button label="Add" />
+      <Button label="Add" onClick={handleClick} />
 
        <CircleGraph value={ 1 / 4 }/>
       <Table
@@ -22,11 +26,11 @@ const Addresses = () => {
       </TableRow>
     }
     >
-	{list.map(address => {
+	{whales.map(address => {
 	  return (
 	    <TableRow>
    	      <TableCell>
-		<Link href="https://google.com">{address}</Link>
+		<Link href={`https://etherscan.io/address/${address}`}>{address}</Link>
 	      </TableCell>
 	    </TableRow>
 	  )
