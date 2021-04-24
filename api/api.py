99141107c76f041b84630f69427de5f45174d6f2
MountVase <markus.vasemagi@gmail.com>
2021-04-24 19:07:52 +0300

    add flask api

---
+++ api/api.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+
+cors = CORS(app)
+
+@app.route('/api/getToken/<address>/', methods=['GET'])
+def balance(address):
+     
+    return {
+        'address': address,
+        'balance': 123132132
+    }
+
+if __name__ == '__main__':
+    app.run(debug=True)
