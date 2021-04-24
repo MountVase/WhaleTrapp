5807bc50a61c7c65dadbd138f5e14c03f1a0dbce
MountVase <markus.vasemagi@gmail.com>
2021-04-24 21:28:33 +0300

    trying static serving

--- api.py
+++ api.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./build", static_url_path="/")
 
 cors = CORS(app)
 
 # serve static html pages from here?
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
+
+
 @app.route('/api/getToken/<address>/', methods=['GET'])
 def balance(address):
      
@@ -12,6 +17,10 @@ def balance(address):
         'address': address,
         'balance': 123132132
     }
+
+@app.errorhandler(404)
+def not_found(e):
+    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
