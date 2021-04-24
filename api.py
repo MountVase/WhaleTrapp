4d3fdeec3bd633bc14c32df395f15e80e5cf44aa
MountVase <markus.vasemagi@gmail.com>
2021-04-24 21:41:52 +0300

    remove all node things

--- api.py
+++ api.py
@@ -1,14 +1,8 @@
 from flask import Flask
 from flask_cors import CORS, cross_origin
-app = Flask(__name__, static_folder="./build", static_url_path="/")
+app = Flask(__name__)
 
 cors = CORS(app)
-
-# serve static html pages from here?
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
-
 
 @app.route('/api/getToken/<address>/', methods=['GET'])
 def balance(address):
@@ -17,10 +11,6 @@ def balance(address):
         'address': address,
         'balance': 123132132
     }
-
-@app.errorhandler(404)
-def not_found(e):
-    return app.send_static_file('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
